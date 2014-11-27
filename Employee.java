cb1e3ab91c8e5632fdd2764310e8284a9794be72
jaden-young <jaden.young44@gmail.com>
2014-11-27 15:41:49 -0800

    Added Employee class

---
+++ Employee.java
@@ -0,0 +1,64 @@
+package lab10;
+/**
+ *
+ *
+*/
+
+public abstract class Employee{
+
+	
+	//fields
+	String firstName;
+	String firstName;
+	int SSN;
+
+
+/********************************** Accessor Methods **************************/
+
+
+
+	/**
+	 * Returns the first name of the employee
+	 * @return first name of the employee
+	*/
+	public String getFirstName(){
+		return firstName;
+	}
+
+
+
+	/**
+	 * Returns the last name of the employee
+	 * @return last name of the employee
+	*/
+	public String getLastName(){
+		return lastName;
+	}
+
+
+
+	/**
+	 * Returns the Social Security number of the employee
+	 * @return Social security number of the employee
+	*/
+	public int getSSN(){
+		return SSN;
+	}
+	
+
+
+/*******************************************************************************/
+/*********************************** Mutator Methods ***************************/
+
+
+
+	/**
+	 * Sets the first name of the employee
+	 * @param xFirstName new first name for the employee
+	*/
+	public void setFirstName(String xFirstName){
+		
+
+
+
+	
