a102249560dcd63109e2be5478d45b35de97fbee
jaden-young <jaden.young44@gmail.com>
2014-11-27 18:07:29 -0800

    Added SalariedEmployee class

---
+++ SalariedEmployee.java
@@ -0,0 +1,84 @@
+package lab10;
+/**
+ * Subclass of Employee, defines a Salaried employee with a weekly salary.
+ *
+ * <p> Defines a salaried employee that adds a weekly salary to the 
+ * Employee superclass. Default constructor initializes all variables to 
+ * default values. Get/set methods are provided for weekly salary, as well as
+ * toString and equals methods. 
+ * 
+ */
+
+public class SalariedEmployee extends Employee{
+	
+
+
+	//instance variable
+	private double weeklySalary;
+	
+
+
+	/**
+	 * Default constructor, initializes all variables to default values
+	 */
+	public SalariedEmployee(){
+	}
+
+
+
+	/**
+	 * Returns the weekly salary of the employee
+	 * @return Weekly salary of the employee
+	 */
+	public double getWeeklySalary(){
+		return weeklySalary;
+	}
+
+
+
+	/**
+	 * Sets the weekly salary of the employee
+	 * @param xWeeklySalary New weekly salary for the employee
+	 */
+	public void setWeeklySalary(double xWeeklySalary){
+		if(xWeeklySalary > 0)
+			weeklySalary = xWeeklySalary;
+		else
+			throw new IllegalArgumentException("Needs to be a positive " + 
+				"number");
+	}
+
+
+
+	/**
+	 * Returns a printable version of the data contained in the fields of the
+	 * object.
+	 *
+	 * @return String containing first name, last name, SSN, and weekly salary
+	 * 		   of the salaried employee
+	 */
+	@Override toString()
+	public String toString(){
+		String output = "";
+		output += "First name: " + firstName;
+		output += "\nLast name: "+ lastName;
+		output += "\nSocial Security Number: " + SSN;
+		output += "\nWeekly salary: " + getEarnings();
+	}
+
+
+
+	/**
+	 * Compares two SalariedEmployee objects for equality by comparing the 
+	 * data in the fields.
+	 *
+	 * @return True if objects are equal, false if not
+	 */
+	@Override equals()
+	public boolean equals(Object xObj){
+		if(!(xObj instanceof SalariedEmployee))
+			return false;
+		SalariedEmployee
+	}
+
+}
